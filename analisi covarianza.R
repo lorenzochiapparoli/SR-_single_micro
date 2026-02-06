55a932c23d673518d4eecebb94317fa6fecd54f6
Lorenzo Chiapparoli <lorenzochiapparoli1@gmail.com>
2026-02-06 10:07:18 +0100

    primo commit

---
+++ analisi covarianza.R
@@ -0,0 +1,49 @@
+library(tidyr)
+library(dplyr)
+library(broom)  # per tidy() dei risultati dei test
+library(readxl)
+library(writexl)
+library(ggplot2)
+library(openxlsx)
+
+Df <- read_excel("dataset microorganismi singoli.xlsx", sheet = 2) 
+Df$Trial <- factor(Df$Trial)
+
+#ANOVA 
+
+anova_mck <- aov(arc_mck ~ Micro*Trial, data = Df)
+summary(anova_mck)
+
+#ANCOVA 
+
+ancova_ac <- aov(arc_mck ~ Micro*Trial+Acetic+EtOH+`G+F`+Temperatura, data = Df)
+summary(ancova_ac)
+
+# TABELLA varianza spiegata NO RESIDUI
+
+aov_sum <- summary(ancova_ac)[[1]]
+resid_rows <- grep("Residuals", rownames(aov_sum), ignore.case = TRUE)
+aov_sum_no_resid <- aov_sum[-resid_rows, ]
+SS_factors <- sum(aov_sum_no_resid$`Sum Sq`)
+aov_sum_no_resid$VarianzaSpiegata <- aov_sum_no_resid$`Sum Sq` / SS_factors * 100
+aov_sum_no_resid
+aov_sum_no_resid <- as.data.frame(aov_sum_no_resid)
+aov_sum_no_resid
+
+
+Df$arc_mck_adj <- predict(
+  ancova_ac,
+  newdata = transform(
+    Df,
+    Acetic = mean(Acetic, na.rm = TRUE),
+    EtOH   = mean(EtOH,   na.rm = TRUE),
+    `G+F`  = mean(`G+F`,  na.rm = TRUE))
+  )
+
+
+cov_model <- aov(arc_mck ~ Acetic + EtOH + `G+F`, data = Df)
+summary(cov_model)
+Df$arc_mck_ripulito <- residuals(cov_model) + mean(Df$arc_mck)
+anova_rip <- aov(arc_mck_ripulito ~ Micro*Trial, data = Df)
+summary(anova_rip)
+
